460e8e9e37926f69392d95e787c90cf681a1baeb
dreary-merlin <44442423+dreary-merlin@users.noreply.github.com>
2023-09-12 19:47:49 +0300

    part4

---
+++ ui_addbook.h
@@ -0,0 +1,473 @@
+/********************************************************************************
+** Form generated from reading UI file 'addbook.ui'
+**
+** Created by: Qt User Interface Compiler version 5.7.1
+**
+** WARNING! All changes made in this file will be lost when recompiling UI file!
+********************************************************************************/
+
+#ifndef UI_ADDBOOK_H
+#define UI_ADDBOOK_H
+
+#include <QtCore/QVariant>
+#include <QtWidgets/QAction>
+#include <QtWidgets/QApplication>
+#include <QtWidgets/QButtonGroup>
+#include <QtWidgets/QCheckBox>
+#include <QtWidgets/QGridLayout>
+#include <QtWidgets/QHBoxLayout>
+#include <QtWidgets/QHeaderView>
+#include <QtWidgets/QLabel>
+#include <QtWidgets/QLineEdit>
+#include <QtWidgets/QPushButton>
+#include <QtWidgets/QSpacerItem>
+#include <QtWidgets/QTextEdit>
+#include <QtWidgets/QVBoxLayout>
+#include <QtWidgets/QWidget>
+
+QT_BEGIN_NAMESPACE
+
+class Ui_AddBook
+{
+public:
+    QGridLayout *gridLayout;
+    QVBoxLayout *verticalLayout_3;
+    QHBoxLayout *horizontalLayout_17;
+    QVBoxLayout *verticalLayout;
+    QHBoxLayout *horizontalLayout;
+    QLabel *label;
+    QLineEdit *lineEdit;
+    QHBoxLayout *horizontalLayout_2;
+    QLabel *label_2;
+    QLineEdit *lineEdit_2;
+    QHBoxLayout *horizontalLayout_3;
+    QLabel *label_3;
+    QLineEdit *lineEdit_3;
+    QHBoxLayout *horizontalLayout_4;
+    QLabel *label_4;
+    QLineEdit *lineEdit_4;
+    QHBoxLayout *horizontalLayout_5;
+    QLabel *label_5;
+    QLineEdit *lineEdit_5;
+    QHBoxLayout *horizontalLayout_6;
+    QLabel *label_6;
+    QLineEdit *lineEdit_6;
+    QHBoxLayout *horizontalLayout_7;
+    QLabel *label_7;
+    QLineEdit *lineEdit_7;
+    QHBoxLayout *horizontalLayout_8;
+    QLabel *label_8;
+    QLineEdit *lineEdit_8;
+    QVBoxLayout *verticalLayout_2;
+    QHBoxLayout *horizontalLayout_9;
+    QLabel *label_9;
+    QLineEdit *lineEdit_9;
+    QHBoxLayout *horizontalLayout_10;
+    QLabel *label_10;
+    QLineEdit *lineEdit_10;
+    QHBoxLayout *horizontalLayout_11;
+    QLabel *label_11;
+    QLineEdit *lineEdit_11;
+    QHBoxLayout *horizontalLayout_12;
+    QLabel *label_12;
+    QLineEdit *lineEdit_12;
+    QHBoxLayout *horizontalLayout_13;
+    QLabel *label_13;
+    QLineEdit *lineEdit_13;
+    QHBoxLayout *horizontalLayout_14;
+    QLabel *label_14;
+    QCheckBox *checkBox;
+    QHBoxLayout *horizontalLayout_15;
+    QLabel *label_15;
+    QLineEdit *lineEdit_14;
+    QHBoxLayout *horizontalLayout_16;
+    QLabel *label_16;
+    QLineEdit *lineEdit_15;
+    QHBoxLayout *horizontalLayout_18;
+    QLabel *label_17;
+    QLineEdit *lineEdit_16;
+    QPushButton *pushButton_3;
+    QLabel *label_18;
+    QTextEdit *textEdit;
+    QLabel *label_19;
+    QTextEdit *textEdit_2;
+    QHBoxLayout *horizontalLayout_20;
+    QPushButton *pushButton;
+    QSpacerItem *horizontalSpacer;
+    QPushButton *pushButton_2;
+
+    void setupUi(QWidget *AddBook)
+    {
+        if (AddBook->objectName().isEmpty())
+            AddBook->setObjectName(QStringLiteral("AddBook"));
+        AddBook->resize(754, 797);
+        QFont font;
+        font.setPointSize(10);
+        AddBook->setFont(font);
+        QIcon icon;
+        icon.addFile(QString::fromUtf8(":/new/prefix1/\320\270\320\267\320\276\320\261\321\200\320\260\320\266\320\265\320\275\320\270\320\265_2023-04-10_135944216.png"), QSize(), QIcon::Normal, QIcon::Off);
+        AddBook->setWindowIcon(icon);
+        gridLayout = new QGridLayout(AddBook);
+        gridLayout->setObjectName(QStringLiteral("gridLayout"));
+        verticalLayout_3 = new QVBoxLayout();
+        verticalLayout_3->setObjectName(QStringLiteral("verticalLayout_3"));
+        horizontalLayout_17 = new QHBoxLayout();
+        horizontalLayout_17->setObjectName(QStringLiteral("horizontalLayout_17"));
+        verticalLayout = new QVBoxLayout();
+        verticalLayout->setObjectName(QStringLiteral("verticalLayout"));
+        horizontalLayout = new QHBoxLayout();
+        horizontalLayout->setObjectName(QStringLiteral("horizontalLayout"));
+        label = new QLabel(AddBook);
+        label->setObjectName(QStringLiteral("label"));
+
+        horizontalLayout->addWidget(label);
+
+        lineEdit = new QLineEdit(AddBook);
+        lineEdit->setObjectName(QStringLiteral("lineEdit"));
+
+        horizontalLayout->addWidget(lineEdit);
+
+
+        verticalLayout->addLayout(horizontalLayout);
+
+        horizontalLayout_2 = new QHBoxLayout();
+        horizontalLayout_2->setObjectName(QStringLiteral("horizontalLayout_2"));
+        label_2 = new QLabel(AddBook);
+        label_2->setObjectName(QStringLiteral("label_2"));
+
+        horizontalLayout_2->addWidget(label_2);
+
+        lineEdit_2 = new QLineEdit(AddBook);
+        lineEdit_2->setObjectName(QStringLiteral("lineEdit_2"));
+
+        horizontalLayout_2->addWidget(lineEdit_2);
+
+
+        verticalLayout->addLayout(horizontalLayout_2);
+
+        horizontalLayout_3 = new QHBoxLayout();
+        horizontalLayout_3->setObjectName(QStringLiteral("horizontalLayout_3"));
+        label_3 = new QLabel(AddBook);
+        label_3->setObjectName(QStringLiteral("label_3"));
+
+        horizontalLayout_3->addWidget(label_3);
+
+        lineEdit_3 = new QLineEdit(AddBook);
+        lineEdit_3->setObjectName(QStringLiteral("lineEdit_3"));
+
+        horizontalLayout_3->addWidget(lineEdit_3);
+
+
+        verticalLayout->addLayout(horizontalLayout_3);
+
+        horizontalLayout_4 = new QHBoxLayout();
+        horizontalLayout_4->setObjectName(QStringLiteral("horizontalLayout_4"));
+        label_4 = new QLabel(AddBook);
+        label_4->setObjectName(QStringLiteral("label_4"));
+
+        horizontalLayout_4->addWidget(label_4);
+
+        lineEdit_4 = new QLineEdit(AddBook);
+        lineEdit_4->setObjectName(QStringLiteral("lineEdit_4"));
+
+        horizontalLayout_4->addWidget(lineEdit_4);
+
+
+        verticalLayout->addLayout(horizontalLayout_4);
+
+        horizontalLayout_5 = new QHBoxLayout();
+        horizontalLayout_5->setObjectName(QStringLiteral("horizontalLayout_5"));
+        label_5 = new QLabel(AddBook);
+        label_5->setObjectName(QStringLiteral("label_5"));
+
+        horizontalLayout_5->addWidget(label_5);
+
+        lineEdit_5 = new QLineEdit(AddBook);
+        lineEdit_5->setObjectName(QStringLiteral("lineEdit_5"));
+
+        horizontalLayout_5->addWidget(lineEdit_5);
+
+
+        verticalLayout->addLayout(horizontalLayout_5);
+
+        horizontalLayout_6 = new QHBoxLayout();
+        horizontalLayout_6->setObjectName(QStringLiteral("horizontalLayout_6"));
+        label_6 = new QLabel(AddBook);
+        label_6->setObjectName(QStringLiteral("label_6"));
+
+        horizontalLayout_6->addWidget(label_6);
+
+        lineEdit_6 = new QLineEdit(AddBook);
+        lineEdit_6->setObjectName(QStringLiteral("lineEdit_6"));
+
+        horizontalLayout_6->addWidget(lineEdit_6);
+
+
+        verticalLayout->addLayout(horizontalLayout_6);
+
+        horizontalLayout_7 = new QHBoxLayout();
+        horizontalLayout_7->setObjectName(QStringLiteral("horizontalLayout_7"));
+        label_7 = new QLabel(AddBook);
+        label_7->setObjectName(QStringLiteral("label_7"));
+
+        horizontalLayout_7->addWidget(label_7);
+
+        lineEdit_7 = new QLineEdit(AddBook);
+        lineEdit_7->setObjectName(QStringLiteral("lineEdit_7"));
+
+        horizontalLayout_7->addWidget(lineEdit_7);
+
+
+        verticalLayout->addLayout(horizontalLayout_7);
+
+        horizontalLayout_8 = new QHBoxLayout();
+        horizontalLayout_8->setObjectName(QStringLiteral("horizontalLayout_8"));
+        label_8 = new QLabel(AddBook);
+        label_8->setObjectName(QStringLiteral("label_8"));
+
+        horizontalLayout_8->addWidget(label_8);
+
+        lineEdit_8 = new QLineEdit(AddBook);
+        lineEdit_8->setObjectName(QStringLiteral("lineEdit_8"));
+
+        horizontalLayout_8->addWidget(lineEdit_8);
+
+
+        verticalLayout->addLayout(horizontalLayout_8);
+
+
+        horizontalLayout_17->addLayout(verticalLayout);
+
+        verticalLayout_2 = new QVBoxLayout();
+        verticalLayout_2->setObjectName(QStringLiteral("verticalLayout_2"));
+        horizontalLayout_9 = new QHBoxLayout();
+        horizontalLayout_9->setObjectName(QStringLiteral("horizontalLayout_9"));
+        label_9 = new QLabel(AddBook);
+        label_9->setObjectName(QStringLiteral("label_9"));
+
+        horizontalLayout_9->addWidget(label_9);
+
+        lineEdit_9 = new QLineEdit(AddBook);
+        lineEdit_9->setObjectName(QStringLiteral("lineEdit_9"));
+
+        horizontalLayout_9->addWidget(lineEdit_9);
+
+
+        verticalLayout_2->addLayout(horizontalLayout_9);
+
+        horizontalLayout_10 = new QHBoxLayout();
+        horizontalLayout_10->setObjectName(QStringLiteral("horizontalLayout_10"));
+        label_10 = new QLabel(AddBook);
+        label_10->setObjectName(QStringLiteral("label_10"));
+
+        horizontalLayout_10->addWidget(label_10);
+
+        lineEdit_10 = new QLineEdit(AddBook);
+        lineEdit_10->setObjectName(QStringLiteral("lineEdit_10"));
+
+        horizontalLayout_10->addWidget(lineEdit_10);
+
+
+        verticalLayout_2->addLayout(horizontalLayout_10);
+
+        horizontalLayout_11 = new QHBoxLayout();
+        horizontalLayout_11->setObjectName(QStringLiteral("horizontalLayout_11"));
+        label_11 = new QLabel(AddBook);
+        label_11->setObjectName(QStringLiteral("label_11"));
+
+        horizontalLayout_11->addWidget(label_11);
+
+        lineEdit_11 = new QLineEdit(AddBook);
+        lineEdit_11->setObjectName(QStringLiteral("lineEdit_11"));
+
+        horizontalLayout_11->addWidget(lineEdit_11);
+
+
+        verticalLayout_2->addLayout(horizontalLayout_11);
+
+        horizontalLayout_12 = new QHBoxLayout();
+        horizontalLayout_12->setObjectName(QStringLiteral("horizontalLayout_12"));
+        label_12 = new QLabel(AddBook);
+        label_12->setObjectName(QStringLiteral("label_12"));
+
+        horizontalLayout_12->addWidget(label_12);
+
+        lineEdit_12 = new QLineEdit(AddBook);
+        lineEdit_12->setObjectName(QStringLiteral("lineEdit_12"));
+
+        horizontalLayout_12->addWidget(lineEdit_12);
+
+
+        verticalLayout_2->addLayout(horizontalLayout_12);
+
+        horizontalLayout_13 = new QHBoxLayout();
+        horizontalLayout_13->setObjectName(QStringLiteral("horizontalLayout_13"));
+        label_13 = new QLabel(AddBook);
+        label_13->setObjectName(QStringLiteral("label_13"));
+
+        horizontalLayout_13->addWidget(label_13);
+
+        lineEdit_13 = new QLineEdit(AddBook);
+        lineEdit_13->setObjectName(QStringLiteral("lineEdit_13"));
+
+        horizontalLayout_13->addWidget(lineEdit_13);
+
+
+        verticalLayout_2->addLayout(horizontalLayout_13);
+
+        horizontalLayout_14 = new QHBoxLayout();
+        horizontalLayout_14->setObjectName(QStringLiteral("horizontalLayout_14"));
+        label_14 = new QLabel(AddBook);
+        label_14->setObjectName(QStringLiteral("label_14"));
+
+        horizontalLayout_14->addWidget(label_14);
+
+        checkBox = new QCheckBox(AddBook);
+        checkBox->setObjectName(QStringLiteral("checkBox"));
+
+        horizontalLayout_14->addWidget(checkBox);
+
+
+        verticalLayout_2->addLayout(horizontalLayout_14);
+
+        horizontalLayout_15 = new QHBoxLayout();
+        horizontalLayout_15->setObjectName(QStringLiteral("horizontalLayout_15"));
+        label_15 = new QLabel(AddBook);
+        label_15->setObjectName(QStringLiteral("label_15"));
+
+        horizontalLayout_15->addWidget(label_15);
+
+        lineEdit_14 = new QLineEdit(AddBook);
+        lineEdit_14->setObjectName(QStringLiteral("lineEdit_14"));
+
+        horizontalLayout_15->addWidget(lineEdit_14);
+
+
+        verticalLayout_2->addLayout(horizontalLayout_15);
+
+        horizontalLayout_16 = new QHBoxLayout();
+        horizontalLayout_16->setObjectName(QStringLiteral("horizontalLayout_16"));
+        label_16 = new QLabel(AddBook);
+        label_16->setObjectName(QStringLiteral("label_16"));
+
+        horizontalLayout_16->addWidget(label_16);
+
+        lineEdit_15 = new QLineEdit(AddBook);
+        lineEdit_15->setObjectName(QStringLiteral("lineEdit_15"));
+
+        horizontalLayout_16->addWidget(lineEdit_15);
+
+
+        verticalLayout_2->addLayout(horizontalLayout_16);
+
+
+        horizontalLayout_17->addLayout(verticalLayout_2);
+
+
+        verticalLayout_3->addLayout(horizontalLayout_17);
+
+        horizontalLayout_18 = new QHBoxLayout();
+        horizontalLayout_18->setObjectName(QStringLiteral("horizontalLayout_18"));
+        label_17 = new QLabel(AddBook);
+        label_17->setObjectName(QStringLiteral("label_17"));
+
+        horizontalLayout_18->addWidget(label_17);
+
+        lineEdit_16 = new QLineEdit(AddBook);
+        lineEdit_16->setObjectName(QStringLiteral("lineEdit_16"));
+
+        horizontalLayout_18->addWidget(lineEdit_16);
+
+        pushButton_3 = new QPushButton(AddBook);
+        pushButton_3->setObjectName(QStringLiteral("pushButton_3"));
+
+        horizontalLayout_18->addWidget(pushButton_3);
+
+
+        verticalLayout_3->addLayout(horizontalLayout_18);
+
+        label_18 = new QLabel(AddBook);
+        label_18->setObjectName(QStringLiteral("label_18"));
+
+        verticalLayout_3->addWidget(label_18);
+
+        textEdit = new QTextEdit(AddBook);
+        textEdit->setObjectName(QStringLiteral("textEdit"));
+
+        verticalLayout_3->addWidget(textEdit);
+
+        label_19 = new QLabel(AddBook);
+        label_19->setObjectName(QStringLiteral("label_19"));
+
+        verticalLayout_3->addWidget(label_19);
+
+        textEdit_2 = new QTextEdit(AddBook);
+        textEdit_2->setObjectName(QStringLiteral("textEdit_2"));
+
+        verticalLayout_3->addWidget(textEdit_2);
+
+        horizontalLayout_20 = new QHBoxLayout();
+        horizontalLayout_20->setObjectName(QStringLiteral("horizontalLayout_20"));
+        pushButton = new QPushButton(AddBook);
+        pushButton->setObjectName(QStringLiteral("pushButton"));
+
+        horizontalLayout_20->addWidget(pushButton);
+
+        horizontalSpacer = new QSpacerItem(40, 20, QSizePolicy::Expanding, QSizePolicy::Minimum);
+
+        horizontalLayout_20->addItem(horizontalSpacer);
+
+        pushButton_2 = new QPushButton(AddBook);
+        pushButton_2->setObjectName(QStringLiteral("pushButton_2"));
+
+        horizontalLayout_20->addWidget(pushButton_2);
+
+
+        verticalLayout_3->addLayout(horizontalLayout_20);
+
+
+        gridLayout->addLayout(verticalLayout_3, 0, 0, 1, 1);
+
+
+        retranslateUi(AddBook);
+
+        QMetaObject::connectSlotsByName(AddBook);
+    } // setupUi
+
+    void retranslateUi(QWidget *AddBook)
+    {
+        AddBook->setWindowTitle(QApplication::translate("AddBook", "\320\224\320\276\320\261\320\260\320\262\320\273\320\265\320\275\320\270\320\265 \320\275\320\276\320\262\320\276\320\271 \320\272\320\275\320\270\320\263\320\270", Q_NULLPTR));
+        label->setText(QApplication::translate("AddBook", "\320\220\320\262\321\202\320\276\321\200:", Q_NULLPTR));
+        label_2->setText(QApplication::translate("AddBook", "\320\230\320\267\320\264\320\260\321\202\320\265\320\273\321\214\321\201\321\202\320\262\320\276:", Q_NULLPTR));
+        label_3->setText(QApplication::translate("AddBook", "ISBN:", Q_NULLPTR));
+        label_4->setText(QApplication::translate("AddBook", "\320\226\320\260\320\275\321\200:", Q_NULLPTR));
+        label_5->setText(QApplication::translate("AddBook", "\320\246\320\265\320\275\320\260:", Q_NULLPTR));
+        label_6->setText(QApplication::translate("AddBook", "\320\232\320\276\320\273\320\270\321\207\320\265\321\201\321\202\320\262\320\276 \321\215\320\272\320\267\320\265\320\274\320\277\320\273\321\217\321\200\320\276\320\262:", Q_NULLPTR));
+        label_7->setText(QApplication::translate("AddBook", "\320\242\320\270\320\277 \320\272\320\275\320\270\320\263\320\270:", Q_NULLPTR));
+        label_8->setText(QApplication::translate("AddBook", "\320\234\320\265\321\201\321\202\320\276 \321\205\321\200\320\260\320\275\320\265\320\275\320\270\321\217:", Q_NULLPTR));
+        label_9->setText(QApplication::translate("AddBook", "\320\235\320\260\320\267\320\262\320\260\320\275\320\270\320\265:", Q_NULLPTR));
+        label_10->setText(QApplication::translate("AddBook", "\320\223\320\276\320\264 \320\270\320\267\320\264\320\260\320\275\320\270\321\217:", Q_NULLPTR));
+        label_11->setText(QApplication::translate("AddBook", "\320\232\320\276\320\273\320\270\321\207\320\265\321\201\321\202\320\262\320\276 \321\201\321\202\321\200\320\260\320\275\320\270\321\206:", Q_NULLPTR));
+        label_12->setText(QApplication::translate("AddBook", "\320\246\320\270\320\272\320\273:", Q_NULLPTR));
+        label_13->setText(QApplication::translate("AddBook", "\320\224\320\260\321\202\320\260 \320\277\321\200\320\270\320\276\320\261\321\200\320\265\321\202\320\265\320\275\320\270\321\217:", Q_NULLPTR));
+        label_14->setText(QApplication::translate("AddBook", "\320\220\320\275\321\205\320\276\320\273:", Q_NULLPTR));
+        checkBox->setText(QString());
+        label_15->setText(QApplication::translate("AddBook", "\320\241\321\202\320\260\321\202\321\203\321\201:", Q_NULLPTR));
+        label_16->setText(QApplication::translate("AddBook", "\320\232\320\275\320\270\320\266\320\275\320\260\321\217 \320\277\320\276\320\273\320\272\320\260:", Q_NULLPTR));
+        label_17->setText(QApplication::translate("AddBook", "\320\237\321\203\321\202\321\214 \320\272 \320\270\320\267\320\276\320\261\321\200\320\260\320\266\320\265\320\275\320\270\321\216:", Q_NULLPTR));
+        pushButton_3->setText(QApplication::translate("AddBook", "\320\236\321\202\320\272\321\200\321\213\321\202\321\214 \321\204\320\260\320\271\320\273", Q_NULLPTR));
+        label_18->setText(QApplication::translate("AddBook", "\320\241\320\270\320\275\320\276\320\277\321\201\320\270\321\201:", Q_NULLPTR));
+        label_19->setText(QApplication::translate("AddBook", "\320\224\320\276\320\277\320\276\320\273\320\275\320\270\321\202\320\265\320\273\321\214\320\275\320\276:", Q_NULLPTR));
+        pushButton->setText(QApplication::translate("AddBook", "\320\224\320\276\320\261\320\260\320\262\320\270\321\202\321\214", Q_NULLPTR));
+        pushButton_2->setText(QApplication::translate("AddBook", "\320\235\320\260\320\267\320\260\320\264", Q_NULLPTR));
+    } // retranslateUi
+
+};
+
+namespace Ui {
+    class AddBook: public Ui_AddBook {};
+} // namespace Ui
+
+QT_END_NAMESPACE
+
+#endif // UI_ADDBOOK_H
